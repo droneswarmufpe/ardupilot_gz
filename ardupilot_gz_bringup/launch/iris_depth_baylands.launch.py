dc99b965e114a6ae87c12fd18100cbe2a428387c
João Pedro Mafaldo de Paula <jpmp@cin.ufpe.br>
2025-11-11 20:02:30 -0300

    add baylands outdoor launch bringup wip plugin ackermann

---
+++ ardupilot_gz_bringup/launch/iris_depth_baylands.launch.py
@@ -0,0 +1,89 @@
+"""Launch an x500 quadcopter in Gazebo and Rviz."""
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    """Generate a launch description for an x500 quadcopter."""
+    pkg_project_bringup = get_package_share_directory("ardupilot_gz_bringup")
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    # Iris Depth.
+    iris_depth = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("ardupilot_gz_bringup"),
+                        "launch",
+                        "robots",
+                        "iris_depth.launch.py",
+                    ]
+                ),
+            ]
+        ),
+        launch_arguments={
+            "model": "iris_depth",
+            "name": "iris_depth",
+            "x": "0",
+            "y": "0",
+            "z": "0.3",
+            "R": "0.0",
+            "P": "0.0",
+            "Y": "0.0",
+        }.items(),
+    )
+
+    # Gazebo.
+    gz_sim_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={
+            "gz_args": "-v4 -s -r "
+            + f'{Path(pkg_project_gazebo) / "worlds" / "baylands_parking-lot.sdf"}'
+        }.items(),
+    )
+
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
+    )
+
+    # RViz.
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d",
+            f'{Path(pkg_project_bringup) / "rviz" / "iris_with_lidar.rviz"}',
+        ],
+        condition=IfCondition(LaunchConfiguration("rviz")),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            gz_sim_server,
+            gz_sim_gui,
+            iris_depth,
+            rviz,
+        ]
+    )
